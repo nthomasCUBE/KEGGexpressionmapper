1a06f6c0809da3bc324437ed8cc80bb8667a46dc
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2017-06-23 19:51:10 +0200

    Add files via upload

---
+++ R/get_url.R
@@ -0,0 +1,30 @@
+get_url=function(gois){
+                kegg_u=unique(gois[,2])
+                url=""
+                system("rm maps.txt; touch maps.txt")
+                y=0
+                for(x in 1:length(kegg_u)){
+                        if(!is.na(kegg_u[x])){
+                                if(nchar(url)>0){
+                                        url=paste(url,"+","ko:",kegg_u[x],sep="")
+                                }else{
+                                        url=paste(url,"ko:",kegg_u[x],sep="")
+                                }
+                        }
+                        if(y>100){
+                                url=paste("http://www.genome.jp/dbget-bin/get_linkdb?-t+pathway+",url,sep="")
+                                system(paste("wget ",url,sep=""))
+                                system(paste("less get_linkdb* | grep -Po '(map.*)\"' | sed 's/\"//g' | sort | uniq >> maps.txt"))
+                                y=0
+                                url=""
+                        }
+                        y=y+1
+                }
+                url=paste("http://www.genome.jp/dbget-bin/get_linkdb?-t+pathway+",url,sep="")
+                system(paste("wget ",url,sep=""))
+                system(paste("less get_linkdb* | grep -Po '(map.*)\"' | sed 's/\"//g' | sort | uniq >> maps.txt"))
+                system("rm get_linkdb*")
+                system("tmp=$(mktemp); less maps.txt | sort | uniq > $tmp; mv $tmp maps.txt")
+}
+
+
