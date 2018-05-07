6943fd81cdf862648eaf2b2ec047e50e6dafe1e5
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2018-05-07 22:31:41 +0200

    Update get_url.R

--- R/get_url.R
+++ R/get_url.R
@@ -13,7 +13,9 @@ get_url=function(gois){
                         }
                         if(y>10){
                                 url=paste("http://www.genome.jp/dbget-bin/get_linkdb?-t+pathway+",url,sep="")
-                                system(paste("wget ",url,sep=""))
+                                con <- file("stderr.txt", "w"); sink(con, type="message");
+                                system(paste("wget ",url,sep=""),ignore.stdout=TRUE,ignore.stderr=TRUE)
+                                sink(NULL, type="message"); close(con)
                                 system(paste("less get_linkdb* | grep -Po '(map.*)\"' | sed 's/\"//g' | sort | uniq >> maps.txt"))
                                 y=0
                                 url=""
@@ -22,11 +24,11 @@ get_url=function(gois){
 
                 }
                 url=paste("http://www.genome.jp/dbget-bin/get_linkdb?-t+pathway+",url,sep="")
-                system(paste("wget ",url,sep=""))
+                con <- file("stderr.txt", "w"); sink(con, type="message");
+                system(paste("wget ",url,sep=""),ignore.stdout=TRUE,ignore.stderr=TRUE)
+                sink(NULL, type="message"); close(con)
                 system(paste("less get_linkdb* | grep -Po '(map.*)\"' | sed 's/\"//g' | sort | uniq >> maps.txt"))
                 system("rm get_linkdb*")
                 system("tmp=$(mktemp); less maps.txt | sed 's/+/ /g' | cut -f 1 -d ' ' | sort | uniq > $tmp; mv $tmp maps.txt")
 
 }
-
-
